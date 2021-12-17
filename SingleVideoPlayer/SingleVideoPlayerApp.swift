03b70e7db2f7d87a56dad9f45bb030f011e06655
slidstrom <spencerlidstrom@gmail.com>
2021-12-17 10:08:44 -0600

    Initial Commit

---
+++ SingleVideoPlayer/SingleVideoPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SingleVideoPlayerApp.swift
+//  SingleVideoPlayer
+//
+//  Created by DSIAdmin on 12/17/21.
+//
+
+import SwiftUI
+
+@main
+struct SingleVideoPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
